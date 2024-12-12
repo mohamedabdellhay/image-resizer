fe6059622c7412b5f3a0860f4f070593e3684e5f
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2024-12-12 13:35:37 +0200

    add loader

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.getElementById("convertButton").addEventListener("click", async () => {
   const input = document.getElementById("imageInput");
   const output = document.getElementById("output");
-
+  const loader = document.getElementById("loader");
+  const overlay = document.getElementById("overlay");
   if (!input.files.length) {
     alert("Please select images to convert.");
     return;
   }
 
   output.innerHTML = "";
-
+  loader.style.display = "block";
+  overlay.style.display = "block";
+  setTimeout(() => {
+    loader.style.display = "none";
+    overlay.style.display = "none";
+  }, 1000);
   for (const file of input.files) {
     const image = new Image();
     const reader = new FileReader();
@@ -59,7 +65,7 @@ document.getElementById("convertButton").addEventListener("click", async () => {
           link.href = webpUrl;
           link.download = `photos/${file.name.replace(/\.[^/.]+$/, ".webp")}`;
           link.textContent = `Download ${file.name}`;
-          link.className = "btn btn-success d-block my-2";
+          link.className = "all-images btn btn-outline-success d-block my-2";
           input.value = "";
 
           link.addEventListener("click", (e) => {
@@ -69,13 +75,29 @@ document.getElementById("convertButton").addEventListener("click", async () => {
             a.download = `photos/${file.name.replace(/\.[^/.]+$/, ".webp")}`;
             a.click();
           });
-
           output.appendChild(link);
           resolve();
         });
       };
     });
   }
+  // download all images
+  const downloadAll = document.createElement("button");
+  downloadAll.textContent = "Download All";
+  downloadAll.className = "btn btn-success d-block w-100";
+
+  output.insertAdjacentElement("afterbegin", downloadAll);
+  downloadAll.addEventListener("click", function () {
+    const links = document.querySelectorAll(".all-images");
+    links.forEach((ele) => {
+      const a = document.createElement("a");
+      a.href = ele.href;
+      a.download = `photos/${ele.textContent
+        .trim()
+        .replace(/\.[^/.]+$/, ".webp")}`;
+      a.click();
+    });
+  });
 });
 
 // prevent users from refreshing the page
